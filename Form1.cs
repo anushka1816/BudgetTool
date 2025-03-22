618bca97559696b8646cf903c87240bcfebb79b7
anushka1816 <agupt186@gmail.com>
2025-03-22 09:43:55 -0500

    Designed the visuals for the home page.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace BudgetTool
         {
             InitializeComponent();
         }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
